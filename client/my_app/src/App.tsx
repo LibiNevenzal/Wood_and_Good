569e8da901b69e81976aee28c3689fa33e54ec19
libi briz <libibriz@gmail.com>
2024-12-04 18:34:08 +0200

    add order page

--- client/my_app/src/App.tsx
+++ client/my_app/src/App.tsx
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Order from './components/Home/Order/Order';
 
 function App() {
   return (
-    
-    <Home></Home>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/order" element={<Order/>}/>
+      </Routes>
+    </Router>
+  
   );
 }
 
--- client/my_app/src/components/Home/Home.tsx
+++ client/my_app/src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 
 interface HomeProps {}
@@ -7,6 +8,22 @@ const Home: FC<HomeProps> = () => {
    return(
       <div>
       <h1>wood and good</h1>
+      <nav>
+        <ul style={{ display: 'flex', listStyleType: 'none' }}>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/knowledge">ידע על עץ</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/order">להזמנה</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/images">תמונות של מוצרים קיימים</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/cart">הסל שלי</Link>
+          </li>
+        </ul>
+      </nav>
       </div>
        );
 }
