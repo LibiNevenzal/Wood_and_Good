d235d8abb6d34726afaab6ba4e1b8fe48e404434
libi briz <libibriz@gmail.com>
2024-12-04 15:15:45 +0200

    create client

---
+++ client/my_app/src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+   return(
+      <div>
+     <h1>woodyyy</h1>
+      </div>
+       );
+}
+
+
+export default Home;
