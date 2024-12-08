201a0e70cb54762585c627098f70aee40069f118
libi briz <libibriz@gmail.com>
2024-12-08 14:07:11 +0200

    add log_in

--- client/my_app/src/App.tsx
+++ client/my_app/src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Order from './components/Home/Order/Order';
+import Layout from './components/Home/Layout/Layout';
+import SignupForm from './components/SignupForm/SignupForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<SignupForm/>}/>
         <Route path="/order" element={<Order/>}/>
       </Routes>
     </Router>
